e222a1ec582e8613e39e19d6a44e62df4b0fd34c
Lypso28 <tyler.wist@gmx.de>
2026-06-26 23:13:16 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,18 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "DEINE_API_KEY",
-  authDomain: "DEIN_PROJEKT.firebaseapp.com",
-  projectId: "DEIN_PROJEKT_ID",
-  storageBucket: "DEIN_PROJEKT.appspot.com",
-  messagingSenderId: "DEINE_ID",
-  appId: "DEINE_APP_ID"
+  apiKey: "AIzaSyBe96l-zFoKeGV7iuzW1AOGTJrRS96MCSs",
+  authDomain: "money-empire-server.firebaseapp.com",
+  projectId: "money-empire-server",
+  storageBucket: "money-empire-server.firebasestorage.app",
+  messagingSenderId: "488996031863",
+  appId: "1:488996031863:web:a7470bd86b8ddabfad2572"
 };
 
+// Initialisiere Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exportiere die Datenbank (db), damit script.js darauf zugreifen kann
 export const db = getFirestore(app);
