3e2475c8d412127e9546a62dde19b44e192e3f02
Lypso28 <tyler.wist@gmx.de>
2026-06-27 21:29:25 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBe96l-zFoKeGV7iuzW1AOGTJrRS96MCSs",
@@ -8,11 +9,13 @@ const firebaseConfig = {
   projectId: "money-empire-server",
   storageBucket: "money-empire-server.firebasestorage.app",
   messagingSenderId: "488996031863",
-  appId: "1:488996031863:web:a7470bd86b8ddabfad2572"
+  appId: "1:488996031863:web:a7470bd86b8ddabfad2572",
+  measurementId: "G-N8MG9P3S0T"
 };
 
 // Initialisiere Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// Exportiere die Datenbank (db), damit script.js darauf zugreifen kann
+// Exportiere die Datenbank, damit dein Spiel sie nutzen kann
 export const db = getFirestore(app);
